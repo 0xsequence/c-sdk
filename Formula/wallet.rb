bdf1f9ed312fe59cb133213d6db4eddcbb56def0
Andy Grüning <andygruening@gmail.com>
2026-03-19 13:10:59 +0100

    update formula sha

--- Formula/wallet.rb
+++ Formula/wallet.rb
@@ -1,8 +1,8 @@
 class Wallet < Formula
   desc "Sequence C SDK"
   homepage "https://github.com/0xsequence/c-sdk"
-  url "https://github.com/0xsequence/c-sdk/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "SHA256_OF_TARBALL"
+  url "https://github.com/0xsequence/c-sdk/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "d891458dba853c3fb52d5170bb07453556adb1583df1587fcd900b46e3f729c2"
   license "MIT"
 
   depends_on "cmake" => :build
