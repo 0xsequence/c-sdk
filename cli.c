878d21b6d7f4055d7fd03b3e234fac8abd2f7d2d
Andy Grüning <andygruening@gmail.com>
2026-04-01 10:25:46 +0200

    fixed help command for create-wallet

--- cli.c
+++ cli.c
@@ -77,6 +77,7 @@ void print_help(const char *prog) {
        "\n"
        "  create-wallet\n"
        "      Create a new wallet\n"
+       "      --wallet-type <wallet-type> (optional)\n"
        "\n"
        "  sign-message\n"
        "      Sign a message\n"
